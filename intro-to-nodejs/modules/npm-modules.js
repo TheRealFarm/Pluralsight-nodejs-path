9aa944e0f77ad01a21a3d990feda6285ca9b7584
TheRealFarm <kfarm12@uw.edu>
2018-04-04 15:03:04 -0700

    initial commit to keep track of learning milestones

--- intro-to-nodejs/modules/npm-modules.js
+++ intro-to-nodejs/modules/npm-modules.js
@@ -1,7 +1,15 @@
 var request = require('request');
 
-request('http://www.pluralsight.com/', function(err, response, body){
-  if (!err && response.statusCode == 200){
-    console.log(body);
+function getJSONData(callback){
+  request('https://api.coinmarketcap.com/v1/ticker/?limit=5', function(err, response, body){
+    if (!err && response.statusCode == 200){
+      return callback(JSON.parse(body));
+    }
+  });
+}
+
+getJSONData(function(data) {
+  for (var i = 0; i < data.length; i++){
+    console.log("$" + data[i].market_cap_usd.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'));
   }
 });
